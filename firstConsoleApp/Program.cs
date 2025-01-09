94bcc2ad34f219fd89dc6eb978b4b05bffb79d24
A-Hicken <115915984+A-Hicken@users.noreply.github.com>
2025-01-08 17:36:36 -0700

    Add files via upload

--- firstConsoleApp/Program.cs
+++ firstConsoleApp/Program.cs
@@ -1,10 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using System;
+using System.Formats.Asn1;
 
 namespace firstConsoleApp;
 
+
 //Class Declaration
-public class Name
+public class Person
+
 {
     static void Main(string[] args)
     {
@@ -22,5 +26,13 @@ public class Name
 
         //Print the value of the variable (location), which will display in the input value
         Console.WriteLine($"I have never been to {location}. I bet it is nice.");
+
+        MeasureClass measurement = new MeasureClass();
+        measurement.MeasureNumbers();
+
+        ChristmasCountDown christmasCountDown = new ChristmasCountDown();
+        christmasCountDown.DaysToChristmas();
     }
+
+ 
 }
